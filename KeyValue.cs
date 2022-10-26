5cdfb6bcbcd78aa3d8fbe788bbc312b620cffe08
himanshu01031997 <hs010397@gmail.com>
2022-10-26 16:54:10 +0530

    to find word

---
+++ KeyValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_Day14
+{
+    public class KeyValue<K,V>
+    {
+
+        public K? Key { get; set; }
+        public V? Value { get; set; }
+    }
+}
---
+++ MyMapNode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_Day14
+{
+    public class MyMapNode<K, V>//generics
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;//formula
+            return Math.Abs(position);//to return absolute value
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K,V> items in linkedlist)
+            {
+                if (items.Key.Equals(key))
+                {
+                    return items.Value;
+                }
+            }
+            return default(V);
+        }
+         
+        public void Add(K key,V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K,V>> linklist =GetLinkedList(position);
+            KeyValue<K,V> items = new KeyValue<K, V> { Key = key, Value = value };
+            linklist.AddLast(items);
+        }
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+       
+    }
+}   
